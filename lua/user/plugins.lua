21379024730cf25211ba11a21236157c7b5949b9
Sucipto <sucipto@pm.me>
2022-03-24 11:04:42 +0700

    copilot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -101,6 +101,7 @@ return packer.startup(function(use)
 		config = function()
 			vim.g.copilot_assume_mapped = true
 			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_tab_fallback = ""
 		end,
 	})
 
