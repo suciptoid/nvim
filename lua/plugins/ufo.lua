8b206fe0cc48664d91a2bffcb319724c16d1b282
Sucipto <sucipto@pm.me>
2023-03-12 11:54:21 +0700

    customize statuscol for foldcolumn (ufo)

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -1,5 +1,3 @@
-
--- stylua: ignore
 -- if true then return {} end
 
 return {
@@ -8,10 +6,32 @@ return {
     event = "BufRead",
     dependencies = {
       { "kevinhwang91/promise-async" },
+      {
+        "luukvbaal/statuscol.nvim",
+        config = function()
+          local builtin = require("statuscol.builtin")
+          require("statuscol").setup({
+            -- foldfunc = "builtin",
+            -- setopt = true,
+            relculright = true,
+            segments = {
+              { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+              { text = { "%s" }, click = "v:lua.ScSa" },
+              { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+            },
+          })
+        end,
+      },
     },
     config = function()
-      require('ufo').setup()
+      -- Fold options
+      vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+      vim.o.foldcolumn = "1" -- '0' is not bad
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
 
+      require("ufo").setup()
     end,
   },
 }
