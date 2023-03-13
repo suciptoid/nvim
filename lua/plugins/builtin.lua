313c945d21c9d1d73dfe1db1beddd438322f2257
Sucipto <sucipto@pm.me>
2023-03-13 12:50:24 +0700

    use eslint_d

--- lua/plugins/builtin.lua
+++ lua/plugins/builtin.lua
@@ -3,5 +3,5 @@ return {
   { import = "lazyvim.plugins.extras.lang.typescript" },
   { import = "lazyvim.plugins.extras.lang.json" },
   { import = "lazyvim.plugins.extras.formatting.prettier" },
-  { import = "lazyvim.plugins.extras.linting.eslint" },
+  -- { import = "lazyvim.plugins.extras.linting.eslint" },
 }
--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -27,6 +27,7 @@ return {
         "shellcheck",
         "shfmt",
         "emmet-ls",
+        "eslint_d",
         "intelephense",
         "php-cs-fixer",
         "vue-language-server",
@@ -147,9 +148,9 @@ return {
         sources = {
           -- JS Stuff
           -- nls.builtins.formatting.prettierd,
-          -- nls.builtins.formatting.eslint_d,
-          -- nls.builtins.code_actions.eslint_d,
-          -- nls.builtins.diagnostics.eslint_d,
+          nls.builtins.formatting.eslint_d,
+          nls.builtins.code_actions.eslint_d,
+          nls.builtins.diagnostics.eslint_d,
           -- Lua stuff
           nls.builtins.formatting.stylua,
           -- PHP stuff (no longer need this, since intelephense configured properly with shiftwidth=4)
